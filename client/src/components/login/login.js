8f8c7609be954f96da6e07fd8a2a14f3919c7c51
ramsai <ramsaiandhavarapu07@gmail.com>
2023-09-10 23:42:51 +0530

    login and registration

---
+++ client/src/components/login/login.js
@@ -0,0 +1,23 @@
+import { NavBar } from "../navbar/navbar";
+
+export const Login=()=>{
+    return(
+        <>
+        <NavBar/>
+        <section className="container">
+        <header>Login</header>
+        <form action="#" className="form">
+          <div className="input-box">
+            <label>Email Address</label>
+            <input type="text" placeholder="Enter email address" required />
+          </div>
+          <div className="input-box">
+            <label>Password</label>
+            <input type="password" placeholder="Enter Password" required />
+          </div>
+          <button>Submit</button>
+        </form>
+      </section>
+        </>
+    );
+}
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,48 +1,73 @@
-export const NavBar =()=>{
-    return(
-        <>
-        <div className="header_bottom">
-        <div className="container-fluid">
-          <nav className="navbar navbar-expand-lg custom_nav-container ">
-            <a className="navbar-brand " href="index.html"> Finter </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className> </span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav  ">
-                <li className="nav-item active">
-                  <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html"> About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="service.html">Services</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="team.html"> Team </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contact.html">Contact Us</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    <i className="fa fa-user" aria-hidden="true" />
-                    <span>
-                      Login
-                    </span>
-                  </a>
-                </li>
-                <form className="form-inline justify-content-center">
-                  <button className="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                    <i className="fa fa-search" aria-hidden="true" />
-                  </button>
-                </form>
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </div>
-        </>
-    );
+// import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import './navbar.css'
+export const NavBar=()=> {
+  return (
+  <>
+    {['sm'].map((expand) => (
+      <Navbar key={expand} expand={expand} className=" mb-3 navbar">
+        <Container fluid >
+        <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="client\src\components\images\mainlogo.jpg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Project Uploder
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement="end"
+            className="navbaroffcanvas"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Project Uploder
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Login</Nav.Link>
+                <Nav.Link href="#action2">Register</Nav.Link>
+                <NavDropdown
+                  title="Projects"
+                  id={`offcanvasNavbarDropdown-expand-${expand}`}
+                >
+                  <NavDropdown.Item href="#action3">College</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    University
+                  </NavDropdown.Item>
+                  {/* <NavDropdown.Divider /> */}
+                  <NavDropdown.Item href="#action5">
+                    Others
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                {/* <Button variant="outline-success">Search</Button> */}
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    ))}
+    </>
+  );
 }
+
